fd116b2a6beaf4e9c22b17b686d3b6e3d6c5646c
miniweeb <dtngoc.study@gmail.com>
2026-08-02 17:00:44 +0700

    feat[content]: update timeline

--- src/features/event/eventConfig.ts
+++ src/features/event/eventConfig.ts
@@ -1,10 +1,10 @@
 export const EVENT_CONFIG = {
   title: 'Sweet 21st Birthday',
-  partyName: "Nhi's Birthday Party",
+  partyName: "Nhis's Birthday Party",
   hostName: 'Nhis',
   age: 21,
   date: new Date('2026-08-12T00:00:00+07:00'),
-  displayDate: '12/08/2026',
+  displayDate: '15/08/2026',
   startTime: '18:00',
   venue: 'Haidilao Gigamall',
   address: '240 - 242 Phạm Văn Đồng, Hiệp Bình, TP.HCM',
